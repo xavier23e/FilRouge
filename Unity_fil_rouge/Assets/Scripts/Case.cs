ef12c139f215e5bda4a4a4a0ad85836b799c6d82
Kevin-Renevot <renevotkevin@yahoo.fr>
2015-03-04 16:49:14 +0100

    Ajout grille de jeu (Unity)

---
+++ Unity_fil_rouge/Assets/Scripts/Case.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Case : MonoBehaviour {
+
+	private GameObject item;
+	//[HideInInspector]
+	public bool actif;
+	private bool placed;
+
+	// Use this for initialization
+	void Awake () {
+		item = GameObject.Find ("Item");
+		Debug.Log ("item=" + item.name);
+		actif = true;
+		placed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Au survol de la souris
+	void OnMouseOver() {
+		if (actif && !placed) {
+			//renderer.material.color = Color.red;
+			item.transform.position = transform.position;
+		}
+	}
+
+	void OnMouseExit() {
+		if (actif) {
+			renderer.material.color = Color.white;
+		}
+	}
+
+	void OnMouseDown () {
+		placed = true;
+		Debug.Log ("Item placed");
+	}
+}
---
+++ Unity_fil_rouge/Assets/Scripts/Texture.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Texture : MonoBehaviour {
+
+	public Texture2D map;
+
+	public GameObject grille;
+
+	private int row;
+	private int column;
+
+	// Use this for initialization
+	void Start () {
+
+		row = 0;
+		column = 0;
+
+		foreach (Transform cellule in grille.transform) {
+			Debug.Log("Couleur du pixel("+row+", "+column+") = " + map.GetPixel(row, column));
+			//Debug.Log(cellule.name);
+			// Si le pixel courant de l'image est noir
+			if (map.GetPixel(row, column) == Color.black) {
+				// On rend le script inactif pour empecher la construction
+				cellule.GetComponent<Case>().actif = false;
+				cellule.renderer.material.color = Color.black;
+			}
+
+			// Incrémentation de la colonne
+			column++;
+
+			// Si on est à la dernière colonne
+			if(column >= map.width) {
+				// On revient à la première colonne
+				column = 0;
+				// On incrémente la ligne
+				row++;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
