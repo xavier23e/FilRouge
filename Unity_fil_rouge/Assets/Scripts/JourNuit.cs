963a7b517307fc01ff71fe1c4b968da4e8685ee4
UnityDev <ravet.antoine.eln@gmail.com>
2015-03-11 16:23:37 +0100

    Modif jour/nuit amélio map

---
+++ Unity_fil_rouge/Assets/Scripts/JourNuit.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JourNuit : MonoBehaviour {
+	public GameObject etoiles;
+	public Material jourSky;
+	public Material NightSky;
+	public bool timeDay;
+	private float timer;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		timeDay = false;
+		timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer = timer + Time.deltaTime;
+		
+		if (timer > 10 ) {
+			
+			if (timeDay == false){
+				print("jour");
+				timeDay = true;
+			}
+			else
+			{
+				timeDay=false;
+				print("nuit");
+			}
+			timer = 0f;
+		}
+		
+		
+		// Gestion du ciel Jour/Nuit
+		if ( timeDay == false){
+			RenderSettings.skybox = NightSky;
+		}
+		else {
+			RenderSettings.skybox = jourSky;
+		}
+	}
+}
