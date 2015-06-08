3c96f16c898dc39e85da34dbb91c785190d2d39f
xavier23e <xavier@23e.fr>
2015-06-08 15:43:48 +0200

    20150608-1543

--- Unity_fil_rouge/Assets/Scripts/readxml.cs
+++ Unity_fil_rouge/Assets/Scripts/readxml.cs
@@ -5,17 +5,14 @@ using System.Xml;
 using System.IO;
 
 public class readxml : MonoBehaviour {
-{
-	public TextAsset GameAsset;
-	
-	public string Question = "";
-	public string Cylinder_Character = "";
+
+/*  public TextAsset GameAsset;
 	
 	List<Dictionary<string,string>> levels = new List<Dictionary<string,string>>();
 	Dictionary<string,string> obj;
 	
 	void Start()
-	{ //Timeline of the Level creator
+	{
 		GetQuestion();
 	}
 	
@@ -24,7 +21,8 @@ public class readxml : MonoBehaviour {
 	{
 		XmlDocument xmlDoc = new XmlDocument(); // xmlDoc is the new xml document.
 		xmlDoc.LoadXml(GameAsset.text); // load the file.
-		XmlNodeList questionsList = xmlDoc.GetElementsByTagName("level"); // array of the level nodes.
+		XmlNodeList questionsList = xmlDoc.GetElementsByTagName("question"); // array of the level nodes.
+		
 		
 		foreach (XmlNode question in questionsList)
 		{
@@ -38,28 +36,8 @@ public class readxml : MonoBehaviour {
 					obj.Add("name",levelsItens.InnerText); // put this in the dictionary.
 				}
 				
-				if(levelsItens.Name == "tutorial")
-				{
-					obj.Add("tutorial",levelsItens.InnerText); // put this in the dictionary.
-				}
-				
-				if(levelsItens.Name == "object")
-				{
-					switch(levelsItens.Attributes["name"].Value)
-					{
-					case "Cube": obj.Add("Cube",levelsItens.InnerText);break; // put this in the dictionary.
-					case "Cylinder":obj.Add("Cylinder",levelsItens.InnerText); break; // put this in the dictionary.
-					case "Capsule":obj.Add("Capsule",levelsItens.InnerText); break; // put this in the dictionary.
-					case "Sphere": obj.Add("Sphere",levelsItens.InnerText);break; // put this in the dictionary.
-					}
-				}
-				
-				if(levelsItens.Name == "finaltext")
-				{
-					obj.Add("finaltext",levelsItens.InnerText); // put this in the dictionary.
-				}
 			}
 			levels.Add(obj); // add whole obj dictionary in the levels[].
 		}
-	}
+	}*/
 }
